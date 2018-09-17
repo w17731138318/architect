4ffb6b048157b1751f7c576de2781fc22ec53dca
wwx <w17731138318@qq.com>
2018-09-17 17:41:52 +0800

    算法

---
+++ algorithm/src/main/java/com/wwx/algorithm/util/ArrayUtil.java
@@ -0,0 +1,23 @@
+package com.wwx.algorithm.util;
+
+/**
+ * util
+ *
+ * @author 王伟鑫
+ * @version 0.1v
+ * @create 2018-09-17 17:39
+ * @see
+ **/
+public class ArrayUtil {
+	/**
+	 * 数组根据索引换位
+	 * @param arr
+	 * @param i
+	 * @param j
+	 */
+	private static void swap(int[] arr, int i, int j) {
+		int t = arr[i];
+		arr[i] = arr[j];
+		arr[j] = t;
+	}
+}
